900bf4c91320262a26745e47d9afe41bff151810
lizzard85 <90036508+lizzard85@users.noreply.github.com>
2022-06-19 00:17:16 +0200

    added results as comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,17 +8,15 @@ var pascal = CaseConverter.AnalyseCasing("PascalCase");
 var kebab = CaseConverter.AnalyseCasing("kebab-case");
 var snake = CaseConverter.AnalyseCasing("snake_case");
 
-Console.WriteLine($"raw is {raw.Casing}");
-Console.WriteLine($"camel is {camel.Casing}");
-Console.WriteLine($"pascal is {pascal.Casing}");
-Console.WriteLine($"kebab is {kebab.Casing}");
-Console.WriteLine($"snake is {snake.Casing}");
+Console.WriteLine($"raw is {raw.Casing}");       //raw is Unknown
+Console.WriteLine($"camel is {camel.Casing}");   //camel is CamelCase
+Console.WriteLine($"pascal is {pascal.Casing}"); //pascal is PascalCase
+Console.WriteLine($"kebab is {kebab.Casing}");   //kebab is KebabCase
+Console.WriteLine($"snake is {snake.Casing}");   //snake is SnakeCase
 Console.WriteLine();
-Console.WriteLine(CaseConverter.Convert("camelCaseToSnakeCase", Casing.SnakeCase));
-Console.WriteLine(CaseConverter.Convert("PascalCaseToCamelCase", Casing.CamelCase));
-Console.WriteLine(CaseConverter.Convert("snake_case_to_kebab", Casing.KebabCase));
-Console.WriteLine(CaseConverter.Convert("kebab-case-to-pascal-case", Casing.PascalCase));
-
-Console.WriteLine(CaseConverter.Convert("Raw normal text to camel casing", Casing.CamelCase));
-
-Console.WriteLine(CaseConverter.Convert("RawMessed_up_text-toPascalCasing", Casing.PascalCase));
+Console.WriteLine(CaseConverter.Convert("camelCaseToSnakeCase", Casing.SnakeCase));              //camel_case_to_snake_case
+Console.WriteLine(CaseConverter.Convert("PascalCaseToCamelCase", Casing.CamelCase));             //pascalCaseToCamelCase
+Console.WriteLine(CaseConverter.Convert("snake_case_to_kebab", Casing.KebabCase));               //snake-case-to-kebab
+Console.WriteLine(CaseConverter.Convert("kebab-case-to-pascal-case", Casing.PascalCase));        //KebabCaseToPascalCase
+Console.WriteLine(CaseConverter.Convert("Raw normal text to camel casing", Casing.CamelCase));   //rawNormalTextToCamelCasing
+Console.WriteLine(CaseConverter.Convert("RawMessed_up_text-toPascalCasing", Casing.PascalCase)); //RawMessedUpTextToPascalCasing
